9138873572bb743bc6ebd3d33b7f2547a1be8de1
ziaoang <ziaoang@gmail.com>
2016-01-26 00:59:27 +0800

    modify test

--- leleketang/w2v_test.py
+++ leleketang/w2v_test.py
@@ -8,7 +8,7 @@ import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
-'''
+
 fname = "data_filter_seg_merge_split/all_vector.txt"
 model = gensim.models.Word2Vec.load(fname)
 
@@ -19,30 +19,34 @@ for line in open("statistic/all.txt"):
 	if cnt >= 1000:
 		POOL[cy] = cnt
 
-all = 0
+total = 0
 right = 0
+lineNo = 0
 for line in open("data_filter_seg_merge_split/all_test.txt"):
+	lineNo += 1
+	if lineNo % 10000 == 0:
+		print(lineNo)
 	t = line.strip().split("/ ")
-	cy = t[0]
-	for sentence in t[1:]:
-		all += 1
-		words = sentence.strip().split("/ ")
-		cy_index = words.index(cy)
+	for cy in POOL:
+		if cy not in t:
+			continue
+		cy_index = t.index(cy)
 		neighbor = []
 		for i in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
-			if cy_index+i >= 0 and cy_index+i < len(words):
-				if words[i] in word_pool:
-					neighbor.append(words[i])
-		if len(neighbor) != 0:
-			t = model.most_similar(positive=neighbor, negative=[], topn=10)
-			if cy in [a[0] for a in t]:
-				right += 1
-			#if t[0][0] == cy:
-			#	right += 1
+			if cy_index+i >= 0 and cy_index+i < len(t):
+				if t[cy_index+i] in model:
+					neighbor.append(t[cy_index+i])
+		if len(neighbor) == 0:
+			continue
+		total += 1
+		top = model.most_similar(positive=neighbor, negative=[], topn=10)
+		if cy in [a[0] for a in top]:
+			# print(line.strip())
+			right += 1
 
-print(all)
+print(total)
 print(right)
-'''
+
 
 
 

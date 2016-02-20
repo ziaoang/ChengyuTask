7e672f33e2c8ec14bf10dffede2c87a70523ca6f
Chao Lv (MSR Student-Person Consulting) <v-chlv@microsoft.com>
2016-02-20 21:46:43 +0800

    add dataset

--- yaoguo/check.py
+++ yaoguo/check.py
@@ -1,19 +1,32 @@
-
+# encoding=utf-8
 
 import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 import json
 
-
-a = []
+questions = {}
 
 for line in open("data.txt"):
 	content = line.strip()
 	t = json.loads(content)
 	for i in range(15):
 		qid = t["questions"][i]["id"]
-		a.append(qid)
+		if qid not in questions:
+			questions[qid] = t["questions"][i]
 
-print('-'*20)
-print(len(a))
-print(len(set(a)))
-
+for qid in questions:
+	a = questions[qid]["option_a"]
+	b = questions[qid]["option_b"]
+	c = questions[qid]["option_c"]
+	d = questions[qid]["option_d"]
+	if len(a) == 4 and len(b) == 4 and len(c) == 4 and len(d) == 4:
+		print("-"*20)
+		print(questions[qid]["question"])
+		print(questions[qid]["option_a"])
+		print(questions[qid]["option_b"])
+		print(questions[qid]["option_c"])
+		print(questions[qid]["option_d"])
+		print(questions[qid]["answer"])
+		print("\n")
--- yaoguo/yaoguo/spiders/yaoguo.py
+++ yaoguo/yaoguo/spiders/yaoguo.py
@@ -12,8 +12,7 @@ class MininovaSpider(CrawlSpider):
 
 	def __init__(self):
 		for i in range(1000):
-			# self.start_urls.append("http://api.spark.appublisher.com/quizbank/get_note_questions?terminal_type=iOS_phone&app_type=quizbank&app_version=2.4.0&uuid=8CE233A9-D6C9-47C7-931D-14D7D2AA702B&user_id=1146660&user_token=e837c7c710c258d605f108317ecfbc07&timestamp=1454683069.978049&idfa=CB049838-06EA-418E-B4FD-893E052F2151&hierarchy_1=1461&hierarchy_2=103&hierarchy_3=635&type=note")
-			self.start_urls.append("http://api.spark.appublisher.com/quizbank/get_note_questions?terminal_type=iOS_phone&app_type=quizbank&app_version=2.4.0&uuid=8CE233A9-D6C9-47C7-931D-14D7D2AA702B&user_id=1146660&user_token=e837c7c710c258d605f108317ecfbc07&timestamp=1454686078.06518&idfa=CB049838-06EA-418E-B4FD-893E052F2151&hierarchy_1=1461&hierarchy_2=0&hierarchy_3=0&type=note")
+			self.start_urls.append("http://api.spark.appublisher.com/quizbank/get_note_questions?terminal_type=iOS_phone&app_type=quizbank&app_version=2.4.0&uuid=8CE233A9-D6C9-47C7-931D-14D7D2AA702B&user_id=1146660&user_token=e837c7c710c258d605f108317ecfbc07&timestamp=1454683069.978049&idfa=CB049838-06EA-418E-B4FD-893E052F2151&hierarchy_1=1461&hierarchy_2=103&hierarchy_3=635&type=note")
 
 	def parse(self, response):
 		# df = open("data.txt", 'a')
@@ -30,17 +29,4 @@ class MininovaSpider(CrawlSpider):
 			tmp = '{"answer":"%s","category":5,"id":%s,"is_right":true,"duration":1,"note_id":%d}'%(answer, id, note_id)
 			questions.append(tmp)
 		questions = '[' + ','.join(questions) + ']'
-		
-		return FormRequest(url="http://api.spark.appublisher.com/quizbank/submit_paper?terminal_type=iOS_phone&app_type=quizbank&app_version=2.4.0&uuid=8CE233A9-D6C9-47C7-931D-14D7D2AA702B&user_id=1146660&user_token=e837c7c710c258d605f108317ecfbc07&timestamp=1454683245.255333&idfa=CB049838-06EA-418E-B4FD-893E052F2151",
-					   	   formdata={
-							'duration': '15',
-							'paper_id': '%s'%paper_id,
-							'paper_type': 'note',
-							'questions': '%s'%questions,
-							'redo': 'false',
-							'status': 'done'},
-						   callback=self.after_post)
-
-	def after_post(self, response):
-		pass
 		

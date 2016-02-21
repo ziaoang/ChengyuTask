9fe5331736a130032f3586a5b4b27b4be35c424d
Chao Lv (MSR Student-Person Consulting) <v-chlv@microsoft.com>
2016-02-21 09:49:32 +0800

    add new qa

--- yaoguo/qa.py
+++ yaoguo/qa.py
@@ -7,15 +7,57 @@ sys.setdefaultencoding("utf-8")
 import jieba
 jieba.load_userdict("../data/userdict.txt")
 
-lineNo = 0
-for line in open("dataset.txt"):
-	lineNo += 1
-	if (lineNo+7) % 9 == 0:
-		question = line.strip().replace(' ', '')
-		question_seg = jieba.cut(question)
-		if "______" not in question_seg:
-			print("error")
-		# print("/ ".join(question_seg))
+def inner_product(a, b):
+	res = 0
+	for i in range(len(a)):
+		res += a[i] * b[i]
+	return res
+
+model = load_model("../leleketang/data_filter_seg_merge_split/w2v/all_vector_small_sg.txt")
+
+window_size = 5
+
+sf = open("dataset.txt")
+while(1):
+	sf.readline()
+	q_raw = sf.readline().strip()
+	a = sf.readline().strip()
+	b = sf.readline().strip()
+	c = sf.readline().strip()
+	d = sf.readline().strip()
+	answer = sf.readline().strip()
+	sf.readline()
+	sf.readline()
+
+	if a not in model:
+		print(a)
+	if b not in model:
+		print(b)
+	if c not in model:
+		print(c)
+	if d not in model:
+		print(d)
+
+	seg_list = jieba.cut(q_raw)
+	q_split = ("/ ".join(seg_list)).encode("utf-8")
+	q_seg = q_split.split("/ ")
+
+	for i in range(len(q_seg)):
+		if q_seg[i] == '______':
+			context = []
+			for j in range(-window_size, window_size+1):
+				if j == 0 or i+j < 0 or i+j >= len(q_seg):
+					continue
+				w = q_seg[i+j]
+				if w not in model:
+					continue
+				if len(context) == 0:
+					for k in range(len(model[w])):
+						context.append(model[w][k])
+				else:
+					for k in range(len(model[w])):
+						context[i] += model[w][k]
+			break
 
 
 
